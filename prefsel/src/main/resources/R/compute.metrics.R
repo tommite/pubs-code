75fb14c65ac4fe0ec47c8dae0115d19e776489a6
Tommi Tervonen <tervonen@ese.eur.nl>
2013-12-09 18:54:27 +0100

    More to metrics

--- prefsel/src/main/resources/R/compute.metrics.R
+++ prefsel/src/main/resources/R/compute.metrics.R
@@ -1,11 +1,15 @@
 library(hitandrun)
 
+n.samples <- 1E4
+
 ## Variable 'constraints' contains the input, of which each row is a
 ## linear constraint of type ax <= 0
 
-constr <- list(constr=constraints, dir=rep('<=', nrow(constraints)), rhs=rep(0, nrow(constraints)))
+user.constr <- list(constr=constraints, dir=rep('<=', nrow(constraints)), rhs=rep(0, nrow(constraints)))
 
+all.constr <- mergeConstraints(simplexConstraints(ncol(constraints)), user.constr)
 
+chain <- hitandrun(all.constr, n.samples=n.samples)
 
 
 ## Return value has to be list called result, of which each element
